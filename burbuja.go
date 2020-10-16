9b928c581243eed233d54702bdd0e3cabb7ffbe6
Luis Franco <jose.fnavarro@alumnos.udg.mx>
2020-10-15 19:22:37 -0500

    Programa para ordenar numeros de menor a mayor con el metodo burbuja

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,22 @@
 package main
 
 func Burbuja(s []int64)  {
-
+	elemAComparar := len(s)
+	for i := 0; i < elemAComparar; i++ {
+		if i+1 < len(s){
+			if s[i] > s[i+1]{
+				aux := s[i]
+				s[i] = s[i+1]
+				s[i+1] = aux
+			}
+			if i+1 == elemAComparar-1{
+				i = -1
+				elemAComparar--	
+			}
+		}
+	}
 }
 
 func main()  {
-	
+
 }
